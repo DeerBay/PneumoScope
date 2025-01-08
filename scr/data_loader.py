f178324be0df650976b20de507d4323e9246f884
SFredin1 <sebbe.fredin1@gmail.com>
2025-01-08 14:27:27 +0100

    import and load data

--- scr/data_loader.py
+++ scr/data_loader.py
@@ -1,1 +1,19 @@
+import kagglehub
+import os
+import shutil
 
+# Define the path to the dataset
+data_folder = os.path.join(os.path.dirname(__file__), "..", "data")
+
+# Ensure the "data" folder exists
+os.makedirs(data_folder, exist_ok=True)
+
+# Download the dataset
+downloaded_path = kagglehub.dataset_download("paultimothymooney/chest-xray-pneumonia")
+
+# Move the downloaded dataset to the "data" folder
+if os.path.exists(downloaded_path):
+    shutil.move(downloaded_path, data_folder)
+    print("Dataset successfully moved to:", data_folder)
+else:
+    print("Error: Dataset was not found at the downloaded path.")
